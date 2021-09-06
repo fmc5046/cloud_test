41926737af5b3f102bb9656227e29c8a36247052
Fred <fmc5046@psu.edu>
2021-09-06 13:11:37 -0400

    different compile

---
+++ print/target/x86_64-unknown-linux-musl/debug/deps/print-c974cf710da4faf5.d
@@ -0,0 +1,5 @@
+/home/fred/School/cloud_test/print/target/x86_64-unknown-linux-musl/debug/deps/print-c974cf710da4faf5: src/main.rs
+
+/home/fred/School/cloud_test/print/target/x86_64-unknown-linux-musl/debug/deps/print-c974cf710da4faf5.d: src/main.rs
+
+src/main.rs:
---
+++ print/target/x86_64-unknown-linux-musl/debug/print.d
@@ -0,0 +1,1 @@
+/home/fred/School/cloud_test/print/target/x86_64-unknown-linux-musl/debug/print: /home/fred/School/cloud_test/print/src/main.rs
